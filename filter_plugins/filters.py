c9bd6a6cb1b28de60bb83b27f5cb6c6bd9f8cfef
Keith Gaughan <k@stereochro.me>
2019-10-05 17:39:41 +0100

    Generate the ACME domain list in a more stable manner.

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,10 +1,11 @@
 def subdomains(domain, labels):
-    return [label + '.' + domain for label in labels]
+    return [label + "." + domain for label in labels]
+
+
+def acme_list(domain, labels):
+    return [domain] + subdomains(domain, labels)
 
 
 class FilterModule(object):
-
     def filters(self):
-        return {
-            'subdomains': subdomains,
-        }
+        return {"subdomains": subdomains, "acme_list": acme_list}
